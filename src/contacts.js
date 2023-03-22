005faa01a3014d96921337522e8a3f54b2d93b3b
tannir02 <tannir02@email.franklin.edu>
2023-03-22 08:52:41 -0500

    homework 5 final commit

---
+++ src/contacts.js
@@ -0,0 +1,22 @@
+const contacts = [
+  {
+    name: "Kemal",
+    imgURL: "https://media.licdn.com/dms/image/C4E03AQE-40IRli5Frg/profile-displayphoto-shrink_200_200/0/1540920894374?e=1684368000&v=beta&t=FO1cANWA7q4nUNZ35HIHCjeiojtVHC6FQkpFbdKxUJg",
+    phone: "+1 (501) 442-0274",
+    email: "kemal.aydin@franklin.edu"
+  },
+  {
+    name: "Kemal",
+    imgURL: "https://media.licdn.com/dms/image/C4E03AQE-40IRli5Frg/profile-displayphoto-shrink_200_200/0/1540920894374?e=1684368000&v=beta&t=FO1cANWA7q4nUNZ35HIHCjeiojtVHC6FQkpFbdKxUJg",
+    phone: "+1 (501) 442-0274",
+    email: "kemal.aydin@franklin.edu"
+  },
+  {
+    name: "Kemal",
+    imgURL: "https://media.licdn.com/dms/image/C4E03AQE-40IRli5Frg/profile-displayphoto-shrink_200_200/0/1540920894374?e=1684368000&v=beta&t=FO1cANWA7q4nUNZ35HIHCjeiojtVHC6FQkpFbdKxUJg",
+    phone: "+1 (501) 442-0274",
+    email: "kemal.aydin@franklin.edu"
+  }
+];
+
+export default contacts;
